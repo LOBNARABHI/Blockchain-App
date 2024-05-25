c3779a17b3ea29d5ab95271b453a8c8643139cfb
LOBNARABHI <lobnarabhi1@gmail.com>
2024-05-25 02:00:49 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
+import {AdminComponent} from "./admin/admin.component";
+import {DeleteComponent} from "./delete/delete.component";
+import {UploadComponent} from "./upload/upload.component";
+import {VerifyComponent} from "./verify/verify.component";
+import {TeamComponent} from "./team/team.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  {path : 'admin' , component : AdminComponent},
+  {path : 'delete' , component : DeleteComponent},
+  {path : 'upload' , component : UploadComponent},
+  {path : 'verify' , component : VerifyComponent},
+  {path : 'team' , component : TeamComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { DeleteComponent } from './delete/delete.component';
+import { UploadComponent } from './upload/upload.component';
+import { VerifyComponent } from './verify/verify.component';
+import { IndexComponent } from './index/index.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DeleteComponent,
+    UploadComponent,
+    VerifyComponent,
+    IndexComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
